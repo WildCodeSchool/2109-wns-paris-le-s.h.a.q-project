8926de17e932ce6b095d56872ad67dfaa9ffbf69
Quentin-HOCKING <hocking.quentin@gmail.com>
2021-10-28 12:10:56 +0200

    init project

---
+++ front-taskmanager/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json',
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
+    "jsx-a11y/label-has-associated-control": [ "error", {
+      "required": {
+        "some": [ "nesting", "id"  ]
+      }
+    }],
+    'react/jsx-props-no-spreading': 'off',
+  },
+  ignorePatterns: ['.eslintrc.js'],
+};
