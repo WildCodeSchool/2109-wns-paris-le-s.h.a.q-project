aa34c1677789e7f1bfe282c19bef3109dbdc6e17
Houra <hu.a.mashaee@gmail.com>
2021-11-24 11:55:11 +0100

    Mutation test in db

--- back-taskmanager/src/resolvers/TaskResolver.test.ts
+++ back-taskmanager/src/resolvers/TaskResolver.test.ts
@@ -10,6 +10,41 @@ beforeAll(async () => {
 });
 
 describe('Task Resolver', () => {
+  describe('mutation createTask', () => {
+    it('add a new task to database', async () => {
+      const createTaskMutation = gql`
+        mutation CreateTask($input: CreateTaskInput!) {
+          createTask(input: $input) {
+            _id
+            subject
+            project
+          }
+        }
+      `;
+
+      const variables = {
+        input: { subject: 'Database', project: 'Task Manager' },
+      };
+
+      const res = await server.executeOperation({
+        query: createTaskMutation,
+        variables,
+      });
+
+      const taskFromResponse = res.data?.createTask;
+
+      expect(taskFromResponse).toEqual(
+        expect.objectContaining({
+          subject: 'Database',
+          project: 'Task Manager',
+        })
+      );
+
+      const taskInDb = await TaskModel.findById(taskFromResponse._id);
+      expect(taskInDb?.subject).toBe(taskFromResponse.subject);
+    });
+  });
+
   describe('get all tasks', () => {
     it('returns all tasks from database', async () => {
       const task1Data = {
--- back-taskmanager/src/resolvers/TaskResolver.ts
+++ back-taskmanager/src/resolvers/TaskResolver.ts
@@ -10,5 +10,29 @@ class TaskResolver {
     const tasks = await TaskModels.find();
     return tasks;
   }
+
+  /*   @Mutation(() => Book)
+async createBook(@Arg("data") data: CreateBookInput) {
+  const book = Book.create(data);
+  await book.save();
+  return book;
+} */
+
+  /* @Mutation(() => Book)
+async updateBook(@Arg("id") id: string, @Arg("data") data: UpdateBookInput) {
+  const book = await Book.findOne({ where: { id } });
+  if (!book) throw new Error("Book not found!");
+  Object.assign(book, data);
+  await book.save();
+  return book;
+} */
+
+  /* @Mutation(() => Boolean)
+async deleteBook(@Arg("id") id: string) {
+  const book = await Book.findOne({ where: { id } });
+  if (!book) throw new Error("Book not found!");
+  await book.remove();
+  return true;
+} */
 }
 export default TaskResolver;
