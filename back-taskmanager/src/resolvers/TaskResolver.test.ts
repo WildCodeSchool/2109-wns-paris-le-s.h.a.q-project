c69bcd6600ef76674e572ee89dc4a13526fc0702
Sofiane <sofiane.c95@gmail.com>
2021-11-24 11:20:03 +0100

    Pour Karim - Cadeau

--- back-taskmanager/src/resolvers/TaskResolver.test.ts
+++ back-taskmanager/src/resolvers/TaskResolver.test.ts
@@ -1,4 +1,5 @@
 import { ApolloServer, gql } from 'apollo-server';
+// import { isType } from 'graphql';
 import createServer from '../createServer';
 import TaskModel from '../models/TaskModels';
 
@@ -6,4 +7,50 @@ let server: ApolloServer;
 
 beforeAll(async () => {
   server = await createServer();
+});
+
+describe('Task Resolver', () => {
+  describe('get all tasks', () => {
+    it('returns all tasks from database', async () => {
+      const task1Data = {
+        subject: 'Front',
+        project: 'Task Manager',
+        description: 'blablabla',
+        assignee: 'sofiane',
+        dueDate: '24/12/21',
+        status: 'to do',
+      };
+      const task2Data = {
+        subject: 'Back',
+        project: 'Task Manager',
+        description: 'blablabla',
+        assignee: 'houra',
+        dueDate: '29/11/21',
+        status: 'doing',
+      };
+      const task1InDb = new TaskModel(task1Data);
+      const task2InDb = new TaskModel(task2Data);
+      await task1InDb.save();
+      await task2InDb.save();
+
+      const allTasksQuery = gql`
+        query allTasks {
+          allTasks {
+            _id
+            subject
+            project
+          }
+        }
+      `;
+      const res = await server.executeOperation({
+        query: allTasksQuery,
+      });
+      expect(res.data?.allTasks).toEqual([
+        expect.objectContaining(task1Data),
+        expect.objectContaining(task2Data),
+      ]);
+
+      expect(res.data?.allTasks[0]._id).toBe(task1InDb._id.toString());
+    });
+  });
 });
