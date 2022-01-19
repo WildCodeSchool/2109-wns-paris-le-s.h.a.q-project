9222c3ad89b77c098098d546c23d85a3f5516be1
Houra <hu.a.mashaee@gmail.com>
2022-01-19 17:08:58 +0100

    docker compose dev ok

--- front-taskmanager/src/index.tsx
+++ front-taskmanager/src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloClient, InMemoryCache, ApolloProvider} from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/",
+  uri: 'http://localhost:5050/graphql',
   cache: new InMemoryCache(),
 });
 
