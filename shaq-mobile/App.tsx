062ab5b57cfe500843fc2fc7d95a646232b02572
Quentin-HOCKING <hocking.quentin@gmail.com>
2022-02-24 15:40:21 +0100

    init project mobile

--- shaq-mobile/App.tsx
+++ shaq-mobile/App.tsx
@@ -1,22 +1,41 @@
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import { extendTheme, NativeBaseProvider } from "native-base";
+import SignUp from "./screens/SignUp";
+import SignIn from "./screens/SignIn";
+import OTP from "./screens/OTP";
+import ProductScreen from "./screens/ProductScreen";
+import StarterIntro from "./screens/StarterIntro";
 
-import useCachedResources from "./hooks/useCachedResources";
-import useColorScheme from "./hooks/useColorScheme";
-import Navigation from "./navigation";
+const config = {
+  useSystemColorMode: false,
+  initialColorMode: "dark",
+};
+
+// extend the theme
+export const theme = extendTheme({ config });
+type MyThemeType = typeof theme;
+declare module "native-base" {
+  interface ICustomTheme extends MyThemeType {}
+}
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+  return (
+    <>
+      <NativeBaseProvider theme={theme}>
+        <NavigationContainer>
+          <Drawer.Navigator screenOptions={{ headerShown: false }}>
+            <Drawer.Screen name={"Home"} component={StarterIntro} />
+            <Drawer.Screen name={"SignUp"} component={SignUp} />
+            <Drawer.Screen name={"SignIn"} component={SignIn} />
+            <Drawer.Screen name={"OTP"} component={OTP} />
+            <Drawer.Screen name={"ProductScreen"} component={ProductScreen} />
+          </Drawer.Navigator>
+        </NavigationContainer>
+      </NativeBaseProvider>
+    </>
+  );
 }
