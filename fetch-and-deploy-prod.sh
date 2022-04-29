6e932f129170d0160d16f991d9cb6c94c1959dff
Sofiane <sofiane.c95@gmail.com>
2022-04-29 11:27:32 +0200

    .gitignore est maintenant fonctionnel

--- fetch-and-deploy-prod.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# fetch-and-deploy-prod.sh
-# docker rm -f
-docker system prune -a --volumes -f && \
-docker-compose -f docker-compose.prod.yml down && \
-git pull origin master && \
-docker-compose -f docker-compose.prod.yml pull && \
-GATEWAY_PORT=8000 docker-compose -f docker-compose.prod.yml up --build -d;
