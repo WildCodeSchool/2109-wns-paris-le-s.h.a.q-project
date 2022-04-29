85a31e9a711d36b890d5849a357681f5715d0fa6
Sofiane <sofiane.c95@gmail.com>
2022-04-29 11:19:44 +0200

    add prune volume on fetch-and-deploy-prod.sh

--- fetch-and-deploy-prod.sh
+++ fetch-and-deploy-prod.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # fetch-and-deploy-prod.sh
+# docker rm -f
+docker system prune -a --volumes -f && \
 docker-compose -f docker-compose.prod.yml down && \
-    docker system prune -a -f && \
-    git pull origin master && \
-    docker-compose -f docker-compose.prod.yml pull && \
-    GATEWAY_PORT=8000 docker-compose -f docker-compose.prod.yml up --build -d;
+git pull origin master && \
+docker-compose -f docker-compose.prod.yml pull && \
+GATEWAY_PORT=8000 docker-compose -f docker-compose.prod.yml up --build -d;
